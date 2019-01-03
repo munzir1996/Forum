af6ec05b816cb1b59beb5af0efaab46638204e73
munzir1996 <munzirmukhtar996@gmail.com>
2019-01-03 14:06:49 +0200

    V1.0

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // $this->call(UsersTableSeeder::class);
+        $threads = factory(\App\Thread::class, 50)->create();
+
+        $threads->each(function($thread){
+            factory(App\Reply::class, 10)->create(['thread_id' => $thread->id]);
+        });
+    }
+}
